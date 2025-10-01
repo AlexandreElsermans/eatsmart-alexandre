702ebfbe14308c540bdb6f691040678d4a99bbfe
Alexandre ELSERMANS <alex.elsermans@gmail.com>
2025-10-01 15:33:12 +0200

    ajout routage MVC
    ajout des fichiers models et controllers en php pour router en MVC

---
+++ alexandre-api-eatsmart/models/articleModel.php
@@ -0,0 +1,25 @@
+<?php
+
+class ArticleModel {
+    private $pdo;
+
+    public function __construct(){
+        try {
+            $this->pdo = new PDO("mysql:host=localhost;dbname=eatsmart_bdd_alexandre;charset=utf8", "root", "");
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion à la base de données : " . $e->getMessage());
+        }
+    }
+    
+    public function getDBAllArticles()
+    {
+        $stmt = $this->pdo->query("SELECT * FROM article");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+//$article1 = new ArticleModel();
+//print_r($article1->getDBAllArticles());
+
+?>
---
+++ alexandre-api-eatsmart/models/categorieModel.php
@@ -0,0 +1,25 @@
+<?php
+
+class CategorieModel {
+    private $pdo;
+
+    public function __construct(){
+        try {
+            $this->pdo = new PDO("mysql:host=localhost;dbname=eatsmart_bdd_alexandre;charset=utf8", "root", "");
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion à la base de données : " . $e->getMessage());
+        }
+    }
+    
+    public function getDBAllCategories()
+    {
+        $stmt = $this->pdo->query("SELECT * FROM categorie");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+//$categorie1 = new CategorieModel();
+//print_r($categorie1->getDBAllCategories());
+
+?>
